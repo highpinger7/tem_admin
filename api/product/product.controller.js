009becdfbd1e02f38e15456e712f3d03d85c3c7a
highpinger <dosrajataveiro07@gmail.com>
2023-08-15 10:31:06 -0700

    new product  model

---
+++ api/product/product.controller.js
@@ -0,0 +1,10 @@
+const { addProduct } = require("./product.service");
+
+module.exports = {
+    addNewProduct:(req,res) => {
+        const body =req.body;
+        // addProduct(body,(error,results))
+    }
+}
+
+function getXlsFileList() {}
--- api/product/product.service.js
+++ api/product/product.service.js
@@ -4,12 +4,17 @@ module.exports = {
   
     addProduct: (data, callback) => {
       pool.query(
-        `insert into product(name,status,is_imp) 
+        `insert into product(picture_map_id,manual_description,accounts_description,model_suitability,specification,part_no,consumer_price,weight,new_consumer_price) 
           values(?,?,?,?,?,?,?,?)`,
         [
-          data.name,
-          data.status,
-          data.is_imp,
+          data.picture_map_id,
+          data.manual_description,
+          data.accounts_description,
+          data.model_suitability,
+          data.specification,
+          data.consumer_price,
+          data.weight,
+          data.new_consumer_price
         ],
         (error, results, field) => {
           if (error) {
